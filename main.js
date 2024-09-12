17071bfa14d47455d95cb03a4eb804175ebc24b5
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-09-12 22:05:49 +0500

    make fuction(getNewHit) & (increaseScore) finish

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+    let timer = 60;
+    let score = 0;
+    let hitrn = 0;
+    function increaseScore(){
+        score += 10;
+        document.querySelector("#scoreval").textContent = score
+    }
+    
+    function getNewHit(){
+      hitrn =  Math.floor(Math.random()*10)
+        document.querySelector("#hitval").textContent =  hitrn
+    }
     function makeBubble  (){
         let clutter = "";
 
@@ -10,7 +22,7 @@
     document.querySelector("#pbtm").innerHTML = clutter
     }
 
-    let timer = 5;
+   
     function runTimer(){
       let timeint =   setInterval(() => {
             if(timer > 0){
@@ -19,10 +31,22 @@
             }
            else{
             clearInterval(timeint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
         }
           
         }, 1000);
     }
+    document.querySelector("#pbtm")
 
+    .addEventListener("click",(dets)=>{
+        let clickednum = Number(dets.target.textContent);
+        if(clickednum === hitrn ){
+            increaseScore();
+            makeBubble();
+            getNewHit();
+        }
+    })
+
+    getNewHit();
     runTimer();
     makeBubble();
